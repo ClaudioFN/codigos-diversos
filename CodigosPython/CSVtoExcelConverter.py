57d10472cf725856a237780d88b284da0f170fa7
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2022-07-23 10:22:50 -0300

    Codigo para converter CSV em Excel
    Commit de codigo que conver arquivos CSV em Excel em formatacao simples alem de criar arquivo de LOG com os status a cada acao efetuada

---
+++ CodigosPython/CSVtoExcelConverter.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import logging
+
+# Configurando o arquivo de LOG
+logging.basicConfig(filename='CSVtoExecelConverter.log', format='%(asctime)s - %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
+print('INICIO - ', datetime.today().strftime('%d-%m-%Y %H:%M:%S'))
+# Ler arquivo CSV
+arquivoCSV = pd.read_csv('arquivo.csv')
+logging.info('\n')
+logging.info('Leu o arquivo')
+
+# Salvar o arquivo para o formato XLSX
+logging.info('Antes de criar novo  arquivo para colocar os dados do formato XLSX')
+nomeArquivoExcel = 'arquivoNovo_' + datetime.today().strftime('%Y-%m-%d') + '.xlsx'
+arquivoExcel = pd.ExcelWriter(nomeArquivoExcel)
+logging.info('Arquivo ' + nomeArquivoExcel)
+
+logging.info('Iniciando a escrita do CSV para o XLSX')
+arquivoCSV.to_excel(arquivoExcel, index = False)
+
+logging.info('Conversao completa!')
+
+arquivoExcel.save()
+logging.info('Arquivo salvo')
+
+print('FIM - ', datetime.today().strftime('%d-%m-%Y %H:%M:%S'))
