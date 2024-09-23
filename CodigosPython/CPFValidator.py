47898dab526e568034f1951643d6367c298b8b8a
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2024-09-22 22:46:52 -0300

    Melhorias no CPFValidator.py

--- CodigosPython/CPFValidator.py
+++ CodigosPython/CPFValidator.py
@@ -2,19 +2,19 @@ import re
 class Documento:
     def valida_documento(documento):
 
-        documento = re.sub(r'[./-]', "", documento) # Substitui os caracteres '.', '/' e '-' por strings vazias ("").
+        documento = re.sub(r'[./-]', "", documento) # Changes the characters '.', '/' and '-' for empty strings ("").
 
-        if len(documento) == 11:
+        if len(documento) == 11: # Validates if the total amount of numbers is equal to the normal CPF quantity
             if re.match(r"[0-9]{11}", documento):
                 return True
             else:
                 return False
-        elif len(documento) == 14:
+        elif len(documento) == 14: # Validates if the total amount of numbers is equal to the normal CNPJ quantity
             if re.match(r"[0-9]{14}", documento):
                 return True
             else:
                 return False
         else:
-            raise print('Quantidade de dígitos está incorreta!!')
+            raise print('Quantidade de dígitos está incorreta!')
 
 
