e85617363267d9eb34c74669cc2c2a9fbe119274
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2024-12-14 12:35:03 -0300

    Inclusao de comentario no arquivo

--- CodigosJavaScript/MainImportation.js
+++ CodigosJavaScript/MainImportation.js
@@ -1,4 +1,9 @@
-
+/*
+Created Date: 21/01/2024
+Last Update: 14/12/2024
+Description: Central Importation
+Observation: Imports FuncoesAuxiliaresImportation to use the definition
+*/
 const { gets, print } = require('./FuncoesAuxiliaresImportation');
 
 print(gets());
