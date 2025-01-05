565aaa6362270d605e4e944b763d020e9968f9d0
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2025-01-04 21:56:22 -0300

    Melhoria de mensagem de descricao do codigo

--- CodigosJava/WEBCrowler/StartWebCrawler.java
+++ CodigosJava/WEBCrowler/StartWebCrawler.java
@@ -1,4 +1,9 @@
-
+/*
+Created Date: 31/07/2022
+Last Update: 04/01/2025
+Description: Creating a web crawler to search throw a specific website
+Observation: Only one class gere StartWebCrawler
+*/
 import java.util.logging.Logger;
 
 public class StartWebCrawler {
