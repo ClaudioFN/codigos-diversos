6a55425e6c21d0c92d0372f3abdd246dc1d8a068
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2025-01-19 13:17:04 -0300

    Melhoria no comentario do codigo

--- CodigosCSharp/api-contatos/Context/AgendaContext.cs
+++ CodigosCSharp/api-contatos/Context/AgendaContext.cs
@@ -1,3 +1,9 @@
+/*
+Created Date: 21/10/2023
+Last Update: 19/01/2025
+Description: Class for items to add new contacts
+Observation: Add context to the of contacts 
+*/
 using API_DIO.Entities;
 using Microsoft.EntityFrameworkCore;
 
