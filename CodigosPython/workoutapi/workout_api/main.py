9d666a772eb30265579e86dac5191073a1d7c844
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2025-03-29 15:06:59 -0300

    Melhoria de mensagens do objeto alterado

--- CodigosPython/workoutapi/workout_api/main.py
+++ CodigosPython/workoutapi/workout_api/main.py
@@ -1,3 +1,10 @@
+'''
+Created Date: 30/06/2024
+Last Update: 29/03/2025
+Description: Main class for the API (making imports from the others parts)
+Observation: Class for the main part of the API and its calling
+'''
+
 from fastapi import FastAPI
 from workout_api.routers import api_router
 
