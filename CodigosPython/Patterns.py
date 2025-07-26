452dab0c56ec76ee7b6a62f2856c27282f4d27d4
Claudio Nascimento <36788885+ClaudioFN@users.noreply.github.com>
2025-07-26 10:50:25 -0300

    Incluindo o padrao da Letra X no arquivo Patterns.py

--- CodigosPython/Patterns.py
+++ CodigosPython/Patterns.py
@@ -139,3 +139,13 @@ for i in range(n):
         else: 
             print(end=" ")
     print()
+
+''' Letter X '''
+n = 7
+for i in range(n):
+    for j in range(n):
+        if(i == j or j == n - 1 - i):
+            print("*", end=" ")
+        else: 
+            print(" ", end=" ")
+    print()
